e41ce5a6fb9023d910bdae233d3ef2593fd5e522
Diego Ramirez <diegojramirezs7@gmail.com>
2020-07-20 21:14:23 -0700

    updated convo retrieval and creation methods

--- phanhoo/phanhooapi/convo_handler.py
+++ phanhoo/phanhooapi/convo_handler.py
@@ -17,17 +17,18 @@ def save_convo_model(convoDic, user_key):
 		# set mainroom, if it doesn't exist create it
 		# other rooms, if they exist add them. Otherwise, create them as well. 
 		current_user = [x for x in User.objects.filter(key=user_key)]
-		mainroom = [x for x in Room.objects.filter(name=convoDic.get('mainroom'))]
+		mainroom = [x for x in Room.objects.filter(name=convoDic.get('mainroom').strip().lower())]
 		
 		tags = save_tags(convoDic.get('tags'))
 
 		if current_user and mainroom:
 			hash_input = convoDic.get('title') + str(current_user[0].id) + convoDic.get('content')
 			mainroom = mainroom[0]
+			current_user = current_user[0]
 			model_dic = {
 				'key': hashlib.sha256(hash_input.encode()).hexdigest(),
 				'title': convoDic.get('title'),
-				'author': current_user[0].id,
+				'author': current_user.id,
 				'content': convoDic.get('content'),
 				'mainroom': mainroom.id,
 				'tags': [x.id for x in tags],
@@ -39,10 +40,18 @@ def save_convo_model(convoDic, user_key):
 				'upvotes': 0,
 				'downvotes': 0
 			}
+
 			serializer = ConvoSerializer(data=model_dic)
 			if serializer.is_valid():
 				serializer.save()
-				return serializer
+
+				serializer_dic = {
+					'serializer': serializer,
+					'user': current_user,
+					'mainroom': mainroom
+				}
+
+				return serializer_dic
 		else:
 			#log
 			return None
@@ -52,15 +61,19 @@ def save_convo_model(convoDic, user_key):
 		return None
 
 
-def prepare_client_response(modelSer):
+def prepare_client_response(serializer_dic):
+	modelSer = serializer_dic.get('serializer')
+	user = serializer_dic.get('user')
+	mainroom = serializer_dic.get('mainroom')
+
 	if modelSer.is_valid():				
 		response_dic = {
 			'key': modelSer.data['key'],
 			'relevantRels': {'created': True},
 			'convoStarter': {
-				'author': modelSer.data['author'].name,
+				'author': {'name': user.name, 'url': user.user_url, 'key': user.key},
 				'title': modelSer.data['title'],
-				'room': modelSer.data['mainroom'].name,
+				'room': {'name': mainroom.name, 'url': mainroom.room_url, 'key': mainroom.key},
 				'content': modelSer.data['content'],
 				'hasImage': not modelSer.data['image'] == '',
 				'image': modelSer.data['image']
@@ -74,3 +87,55 @@ def prepare_client_response(modelSer):
 		}
 
 		return response_dic
+
+
+def convo_list(request):
+	#x = {'name': item.mainroom.name, 'url': item.mainroom.room_url, 'key': item.mainroom.key},
+	if request.method == 'GET':
+		try:
+			user_key = request.headers.get('User-Key')
+			current_user = User.objects.get(key=user_key)
+			#convo = Convo.objects.filter(author=current_user)
+			convos = Convo.objects.all()
+			results = []
+			for item in convos:
+				d = {
+					'key': hashlib.sha256(item.key.encode()).hexdigest(),
+					'relevantRels': {'created': True},
+					'convoStarter': {
+						'author': {'name': item.author.name, 'url': item.author.user_url, 'key': item.author.key},
+						'title': item.title,
+						'room': {'name': item.mainroom.name, 'url': item.mainroom.room_url, 'key': item.mainroom.key},
+						'content': item.content,
+						'hasImage': not item.image == '',
+						'image': item.image
+					},
+					'relatedPosts': {},
+					'convoFooter': {
+						'score': item.score,
+						'upvotes': item.upvotes,
+						'downvotes': item.downvotes,
+					}
+				}
+				results.append(d)
+		except Exception as e:
+			print(str(e))
+			return str(e)
+	elif request.method == 'POST':
+		results = []
+
+	return results
+
+
+"""
+I don't think this needs much explaining, it's just so obvious. 
+Protests reached most countries in the world. 
+Some real changes will occur after this. This is a divisive issue, but it really shouldn't be. 
+I think we can all agree that GF's death was terrible and unfair. 
+However, other suggestions like reparations and such are not really the solution.
+"""
+
+
+
+
+
