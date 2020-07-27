e6b7d7bb275950b5503602b174101101e7d3caf9
Diego Ramirez <diegojramirezs7@gmail.com>
2020-07-27 12:16:17 -0700

    updated style of roomcard

--- frontend/src/components/RoomCard.js
+++ frontend/src/components/RoomCard.js
@@ -3,21 +3,34 @@ import Card from '@material-ui/core/Card';
 import CardContent from '@material-ui/core/CardContent';
 import CardActions from '@material-ui/core/CardActions';
 import Typography from '@material-ui/core/Typography';
-import ArrowDropUpIcon from '@material-ui/icons/ArrowDropUp';
-import AddBoxIcon from '@material-ui/icons/AddBox';
 import IconButton from '@material-ui/core/IconButton';
-
+import BookmarkIcon from '@material-ui/icons/Bookmark';
+import {Link} from 'react-router-dom';
 
 function RoomCard(props) {
 	return (
 		<Card className="roomCard">
 			<CardContent>
-				<h4>{props.room.title}</h4>
+				<Link to="#">
+					<h4>{props.room.title}</h4>
+				</Link>
 				<Typography  variant="body1" component="p">
 				{props.room.description}
 				</Typography>
 			</CardContent>
-			<CardActions>
+			<CardActions className="roomFooter" style={{paddingTop: "0px"}}>
+				<div className="roomFooter">
+					
+					<Typography variant="body1" component="strong" style={{padding: "0px 1vw"}}>
+						Followers:19k
+					</Typography>
+					<Typography variant="body1" component="strong" style={{padding: "0px 1vw"}}>
+						Conversations:71k
+					</Typography>
+					<IconButton size="small" title="save" style={{padding: "0px 1vw"}}>
+						<BookmarkIcon />
+					</IconButton>
+				</div>
 			</CardActions>
 		</Card>
 	);
