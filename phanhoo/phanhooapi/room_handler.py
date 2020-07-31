54122c9b7d75fefe3e0af95f60dd37a4643a53dc
Diego Ramirez <diegojramirezs7@gmail.com>
2020-07-30 22:04:18 -0700

    updated create room response

--- phanhoo/phanhooapi/room_handler.py
+++ phanhoo/phanhooapi/room_handler.py
@@ -1,17 +1,39 @@
-def create_room(room_dic):
+from .serializers import RoomSerializer
+import hashlib
+
+def create_room(room_dic, current_user):
 	# key, name, room_url, author, description, created, score, 
 	# rank, followers, tags
-	preprocessed_name = convoDic.get('mainroom').strip().lower()
-	url_string = preprocessed_name.replace(" ", "-")
-	room_dic = {
-		'key': hashlib.sha256(preprocessed_name.encode()).hexdigest(),
-		'name': preprocessed_name,
-		'room_url': url_string,
-		'author': author.id if author else None
-		'description': "",
-		'score': 0,
-		'rank': 0
-	}
+	try:
+		preprocessed_name = room_dic.get('title').strip().lower()
+		url_string = preprocessed_name.replace(" ", "-")
+		hash_input = room_dic.get('title') + str(current_user[0].id) + romm_dic.get('content')
+
+		room_dic = {
+			'key': hashlib.sha256(hash_input.encode()).hexdigest(),
+			'name': preprocessed_name,
+			'room_url': url_string,
+			'author': current_user.id,
+			'description': room_dic.get('content'),
+			'score': 0,
+			'rank': 0
+		}
+		serializer = RoomSerializer(data = room_dic)
+		if serializer.is_valid():
+			serializer.save()
+
+			serializer_dic = {
+				'serializer': serializer,
+				'user': current_user
+			}
+
+			return serializer_dic
+
+		return None
+	except Exception as e:
+		print(str(e))
+		return None
+
 
 
 
