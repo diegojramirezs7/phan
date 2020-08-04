69c7769ec7f4816982c7a149793b5fdef24281ba
Diego Ramirez <diegojramirezs7@gmail.com>
2020-08-03 21:54:29 -0700

    updated user followed feature

--- phanhoo/phanhooapi/user_handler.py
+++ phanhoo/phanhooapi/user_handler.py
@@ -2,19 +2,34 @@ from .serializers import *
 import hashlib
 from .models import Room, User, Convo
 
-def get_main_users(current_users):
+def get_user_rels(user, current_user):
+	try:
+		user_id = current_user.id
+		rels_dic = {
+			'saved': (user.followers.filter(id=user_id).count() != 0),
+			'suggested': False,
+		}
+		return rels_dic
+	except Exception as e:
+		print(str(e))
+		return None
+
+
+
+def get_main_users(current_user):
 	try:
 		users = User.objects.all()
 		results = []
 		for item in users:
-			# relevantRels = get_room_rels(item, current_user)
+			relevantRels = get_user_rels(item, current_user)
 			d = {
 				'key': item.key,
 				'name': item.name,
 				'url': item.user_url,
 				'followers': item.followers.all().count(),
 				'bio': item.bio,
-				'score': item.score
+				'score': item.score,
+				'relevantRels': relevantRels
 			}
 			results.append(d)
 
@@ -22,3 +37,50 @@ def get_main_users(current_users):
 	except  Exception as e:
 		print(str(e))
 		return None
+
+
+def save_user_followed(user, current_user):
+	try:
+		relevantRels = get_user_rels(user, current_user)
+		if relevantRels:
+			if relevantRels.get('saved'):
+				user.followers.remove(current_user)
+			else:
+				user.followers.add(current_user)
+
+			user.save()
+
+		return user
+	except Exception as e:
+		print(str(e))
+		return None
+
+
+def user_updated_response(user, current_user):
+	try:
+		relevantRels = get_user_rels(user, current_user)
+		d = {
+			'key': user.key,
+			'name': user.name,
+			'url': user.user_url,
+			'followers': user.followers.all().count(),
+			'bio': user.bio,
+			'score': user.score,
+			'relevantRels': relevantRels
+		}
+		return d
+	except Exception as e:
+		print(str(e))
+		return None
+
+
+
+
+
+
+
+
+
+
+
+
